98fec5191aa1e493cf21399ddb13002ae812beca
Walquiria Godoy <walquiriagodoy@gmail.com>
2021-11-18 14:25:16 -0300

    feat(dynamic-form): Add dynamic input to select the class

--- contato.js
+++ contato.js
@@ -2,14 +2,31 @@ const criarFieldset = (evento) => {
     evento.preventDefault()
 
     const fieldset = document.querySelector('[data-fieldset]')
-    const opcaoQuinta = '<label for="radio-quinta"><input type="radio" name="turma" value="quinta" id="radio-quinta">Quinta-feira - 20h</label>'
-    const opcaoSabado = '<label for="radio-sabado"><input type="radio" name="turma" value="sabado" id="radio-sabado">Sábado - 18h</label>'
+    console.log(fieldset)
 
-    fieldset.appendChild = opcaoQuinta
-    fieldset.appendChild = opcaoSabado
+    const novaLabel1 = document.createElement('label')
+    const novaLabel2 = document.createElement('label')
+    const opcaoQuinta = '<input type="radio" name="turma" value="quinta" id="radio-quinta">Quinta-feira - 20h'
+    const opcaoSabado = '<input type="radio" name="turma" value="sabado" id="radio-sabado">Sábado - 18h'
+    
+    novaLabel1.innerHTML = opcaoQuinta 
+    novaLabel2.innerHTML = opcaoSabado
+
+    novaLabel1.appendChild = opcaoQuinta 
+    novaLabel2.appendChild = opcaoSabado
+    console.log(novaLabel1)
+    console.log(novaLabel2)
+
+    fieldset.appendChild(novaLabel1) 
+    fieldset.appendChild(novaLabel2)
+   // fieldset.appendChild = opcaoSabado
 
 }
 
 const selecionaForro = document.querySelector('[data-select-forro]')
-selecionaForro.addEventListner('click', criarFieldset)
+selecionaForro.addEventListener('click', criarFieldset)
+
+const selecionaSamba = document.querySelector('[data-select-samba]')
+selecionaSamba.addEventListener('click', criarFieldset)
+
 
