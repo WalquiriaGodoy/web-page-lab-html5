aef620073e621df34337164ec4d0a338c22b8010
Walquiria Godoy <walquiriagodoy@gmail.com>
2021-11-19 00:21:08 -0300

    feat(dynamic-button): Add form message sent

--- contato.js
+++ contato.js
@@ -1,5 +1,6 @@
 import turmasForro from "./componentes/turmasForro.js"
 import turmasSamba from "./componentes/turmasSamba.js"
+import formularioEnviado from "./componentes/formularioEnviado.js"
 
 const selecionaForro = document.querySelector('[data-select-forro]')
 selecionaForro.addEventListener('click', turmasForro)
@@ -7,4 +8,5 @@ selecionaForro.addEventListener('click', turmasForro)
 const selecionaSamba = document.querySelector('[data-select-samba]')
 selecionaSamba.addEventListener('click', turmasSamba)
 
-
+const button = document.querySelector('[data-button]')
+button.addEventListener('click', formularioEnviado)
