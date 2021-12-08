66f65e5a798231d64b7733f80ffed09e820749da
Walquiria Godoy <walquiriagodoy@gmail.com>
2021-12-07 21:21:50 -0300

    feat(save-data): Save data in localStorage and keep them in input

--- contato.js
+++ contato.js
@@ -11,16 +11,69 @@ selecionaSamba.addEventListener('click', turmasSamba)
 const form = document.querySelector('form')
 form.addEventListener('submit', formularioEnviado)
 
-// Adiciona conteúdo do localStorage no input
+// TODO: Refatorar ou compartimentalizar função de armazenamento localStorage
+
+
+
+
+// Adiciona conteúdo do localStorage no input nome
 
 const nomeLocalStorage = localStorage.getItem('nome')
 const inputNome = document.querySelector('[data-tipo="nome"]')
 inputNome.value = nomeLocalStorage
 
-// Salva conteúdo do input no localStorage
+// Salva conteúdo do input nome no localStorage
 
 inputNome.addEventListener('blur', (evento) =>{
     const nome = inputNome.value 
     localStorage.setItem('nome', nome)
 })
 
+
+
+
+// Adiciona conteúdo do localStorage no input email
+
+const emailLocalStorage = localStorage.getItem('email')
+const inputEmail = document.querySelector('[data-tipo="email"]')
+inputEmail.value = emailLocalStorage
+
+// Salva conteúdo do input email no localStorage
+
+inputEmail.addEventListener('blur', (evento) =>{
+    const email = inputEmail.value 
+    localStorage.setItem('email', email)
+})
+
+
+
+
+
+// Adiciona conteúdo do localStorage no input telefone
+
+const telefoneLocalStorage = localStorage.getItem('telefone')
+const inputTelefone = document.querySelector('[data-tipo="telefone"]')
+inputTelefone.value = telefoneLocalStorage
+
+// Salva conteúdo do input telefone no localStorage
+
+inputTelefone.addEventListener('blur', (evento) =>{
+    const telefone = inputTelefone.value 
+    localStorage.setItem('telefone', telefone)
+})
+
+
+
+
+// Adiciona conteúdo do localStorage no input nascimento
+
+const nascimentoLocalStorage = localStorage.getItem('nascimento')
+const inputNascimento = document.querySelector('[data-tipo="nascimento"]')
+inputNascimento.value = nascimentoLocalStorage
+
+// Salva conteúdo do input telefone no localStorage
+
+inputNascimento.addEventListener('blur', (evento) =>{
+    const nascimento = inputNascimento.value 
+    localStorage.setItem('nascimento', nascimento)
+})
